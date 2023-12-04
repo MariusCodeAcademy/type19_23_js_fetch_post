b9495b66722fdfcd792183c6c0d06a7c2ca47dbd
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-04 10:19:42 +0200

    gen form

--- js/create.js
+++ js/create.js
@@ -37,5 +37,27 @@ els.formEl.addEventListener('submit', (event) => {
 });
 
 // A.1 userId tuetu buti selektas su 50 useriu id. sugeneruotu dinamiskai nuo 1 iki 50
-
+//<option value="1">1</option>
+function generateOptions() {
+  for (let i = 1; i < 51; i++) {
+    console.log('i ===', i);
+    // sukurti options el
+    const optionEl = document.createElement('option');
+    // prideti jam value ir texta
+    optionEl.value = i;
+    optionEl.textContent = i;
+    // patalpinti DOM
+    els.userIdEL.append(optionEl);
+  }
+}
+generateOptions();
 // B. tas pats kaip ir A tik kuriam nauja useri kuriam reikia name, email ir addreso su bent 3 savybem. https://jsonplaceholder.typicode.com/users
+const obj = {
+  name: 'Leanne Graham',
+  email: 'Sincere@april.biz',
+  address: {
+    street: 'Kulas Light',
+    suite: 'Apt. 556',
+    city: 'Gwenborough',
+  },
+};
