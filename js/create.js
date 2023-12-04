04cec376d9573ad658a15fdf6d187a06b75d003b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-04 09:54:33 +0200

    posts

--- js/create.js
+++ js/create.js
@@ -1,6 +1,8 @@
 'use strict';
 console.log('create.js file was loaded');
 
+const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
+
 const els = {
   formEl: document.forms[0],
   titleEL: document.getElementById('title'),
@@ -15,6 +17,23 @@ els.formEl.addEventListener('submit', (event) => {
   // sustabdyti forma nuo perkrovimo
   event.preventDefault();
   console.log('forma pateikta');
+  const newPostData = {
+    title: els.titleEL.value,
+    body: els.contentEL.value,
+    userId: +els.userIdEL.value,
+  };
+  console.log('newPostData ===', newPostData);
+
+  fetch(postsUrl, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(newPostData),
+  })
+    .then((resp) => resp.json())
+    .then((rez) => console.log('rez ===', rez))
+    .catch((error) => {
+      console.warn('ivyko klaida:', error);
+    });
 });
 
 // A.1 userId tuetu buti selektas su 50 useriu id. sugeneruotu dinamiskai nuo 1 iki 50
