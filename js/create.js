dd0ecab9be464f18a4871907ad6804740d82c0f2
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-04 08:57:36 +0200

    post uzd

---
+++ js/create.js
@@ -0,0 +1,7 @@
+'use strict';
+console.log('create.js file was loaded');
+
+// A. sukurti forma su inputu title, text, ir userId(selectas)
+// pateikiant forma siusti duomenis i https://jsonplaceholder.typicode.com/posts ir sukurti nauja posta. iskonsolinti atsakyma
+
+// B. tas pats kaip ir A tik kuriam nauja useri kuriam reikia name, email ir addreso su bent 3 savybem. https://jsonplaceholder.typicode.com/users
